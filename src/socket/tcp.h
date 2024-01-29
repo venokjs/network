cfbd848f4b5d2244adaa5ca93243a702fc4bad11
0x303133 <0x303133@proton.me>
2024-01-30 01:24:04 +0300

    fix(socket): Add missing socket headers

--- src/socket/tcp.h
+++ src/socket/tcp.h
@@ -27,7 +27,7 @@ unsigned short v_socket_context_timestamp(int ssl, struct v_socket_context *cont
 
 /* Adds SNI domain and cert in asn1 format */
 void v_socket_context_add_server_name(int ssl, struct v_socket_context *context, const char *hostname_pattern,
-                                      struct v_bun_socket_context_options options, void *user);
+                                      struct v_socket_context_options options, void *user);
 
 void v_socket_context_remove_server_name(int ssl, struct v_socket_context *context, const char *hostname_pattern);
 
@@ -129,5 +129,59 @@ v_socket_context_adopt_socket(int ssl, struct v_socket_context *context, struct 
 /* Create a child socket context which acts much like its own socket context with its own callbacks yet still relies on the
  * parent socket context for some shared resources. Child socket contexts should be used together with socket adoptions and nothing else. */
 struct v_socket_context *v_create_child_socket_context(int ssl, struct v_socket_context *context, int context_ext_size);
+
+/* Returns the underlying native handle for a socket, such as SSL or file descriptor.
+ * In the case of file descriptor, the value of pointer is fd. */
+void *v_socket_get_native_handle(int ssl, struct v_socket *s);
+
+/* Write up to length bytes of data. Returns actual bytes written.
+ * Will call the on_writable callback of active socket context on failure to write everything off in one go.
+ * Set hint msg_more if you have more immediate data to write. */
+int v_socket_write(int ssl, struct v_socket *s, const char *data, int length, int msg_more);
+
+/* Special path for non-SSL sockets. Used to send header and payload in one go. Works like v_socket_write. */
+int v_socket_write2(int ssl, struct v_socket *s, const char *header, int header_length, const char *payload, int payload_length);
+
+/* Set a low precision, high performance timer on a socket. A socket can only have one single active timer
+ * at any given point in time. Will remove any such pre-set timer */
+void v_socket_timeout(int ssl, struct v_socket *s, unsigned int seconds);
+
+/* Set a low precision, high performance timer on a socket. Suitable for per-minute precision. */
+void v_socket_long_timeout(int ssl, struct v_socket *s, unsigned int minutes);
+
+/* Return the user data extension of this socket */
+void *v_socket_ext(int ssl, struct v_socket *s);
+
+/* Return the socket context of this socket */
+struct v_socket_context *v_socket_context(int ssl, struct v_socket *s);
+
+/* Withdraw any msg_more status and flush any pending data */
+void v_socket_flush(int ssl, struct v_socket *s);
+
+/* Shuts down the connection by sending FIN and/or close_notify */
+void v_socket_shutdown(int ssl, struct v_socket *s);
+
+/* Shuts down the connection in terms of read, meaning next event loop
+ * iteration will catch the socket being closed. Can be used to defer closing
+ * to next event loop iteration. */
+void v_socket_shutdown_read(int ssl, struct v_socket *s);
+
+/* Returns whether the socket has been shut down or not */
+int v_socket_is_shut_down(int ssl, struct v_socket *s);
+
+/* Returns whether this socket has been closed. Only valid if memory has not yet been released. */
+int v_socket_is_closed(int ssl, struct v_socket *s);
+
+/* Immediately closes the socket */
+struct v_socket *v_socket_close(int ssl, struct v_socket *s, int code, void *reason);
+
+/* Returns local port or -1 on failure. */
+int v_socket_local_port(int ssl, struct v_socket *s);
+
+/* Returns remote ephemeral port or -1 on failure. */
+int v_socket_remote_port(int ssl, struct v_socket *s);
+
+/* Copy remote (IP) address of socket, or fail with zero length. */
+void v_socket_remote_address(int ssl, struct v_socket *s, char *buf, int *length);
 
 #endif //NETWORK_TCP_H
