36a566244e7d380bdc6be43267f540985a7efedb
0x303133 <0x303133@proton.me>
2024-01-29 19:05:20 +0300

    refactor(socket): Change names

--- src/socket/quic.h
+++ src/socket/quic.h
@@ -18,7 +18,6 @@ struct v_quic_socket_context_options {
     unsigned int cert_count;
     const char **ca;
     unsigned int ca_count;
-
 };
 
 struct v_quic_socket {
@@ -30,66 +29,59 @@ struct v_quic_socket_context;
 struct v_quic_listen_socket;
 struct v_quic_stream;
 
-void *us_quic_stream_ext(struct v_quic_stream *s);
+void *v_quic_stream_ext(struct v_quic_stream *s);
 
-int us_quic_stream_write(struct v_quic_stream *s, char *data, int length);
+int v_quic_stream_write(struct v_quic_stream *s, char *data, int length);
 
-int us_quic_stream_shutdown(struct v_quic_stream *s);
+int v_quic_stream_shutdown(struct v_quic_stream *s);
 
-int us_quic_stream_shutdown_read(struct v_quic_stream *s);
+int v_quic_stream_shutdown_read(struct v_quic_stream *s);
 
-void us_quic_stream_close(struct v_quic_stream *s);
+void v_quic_stream_close(struct v_quic_stream *s);
 
-int us_quic_socket_context_get_header(struct v_quic_socket_context *context, int index, char **name, int *name_length,
-                                      char **value, int *value_length);
+int v_quic_socket_context_get_header(struct v_quic_socket_context *context, int index, char **name, int *name_length,
+                                     char **value, int *value_length);
 
+void v_quic_socket_context_set_header(struct v_quic_socket_context *context, int index, const char *key, int key_length,
+                                      const char *value, int value_length);
 
-void us_quic_socket_context_set_header(struct v_quic_socket_context *context, int index, const char *key, int key_length,
-                                       const char *value, int value_length);
+void v_quic_socket_context_send_headers(struct v_quic_socket_context *context, struct v_quic_stream *s, int num, int has_body);
 
-void us_quic_socket_context_send_headers(struct v_quic_socket_context *context, struct v_quic_stream *s, int num,
-                                         int has_body);
+struct v_quic_socket_context *v_create_quic_socket_context(struct v_loop *loop, struct v_quic_socket_context_options options, int ext_size);
 
-struct v_quic_socket_context *us_create_quic_socket_context(struct us_loop_t *loop, struct v_quic_socket_context_options options,
-                                                            int ext_size);
+struct v_quic_listen_socket *v_quic_socket_context_listen(struct v_quic_socket_context *context, const char *host, int port, int ext_size);
 
-struct v_quic_listen_socket *us_quic_socket_context_listen(struct v_quic_socket_context *context, const char *host, int port, int ext_size);
+struct v_quic_socket *v_quic_socket_context_connect(struct v_quic_socket_context *context, const char *host, int port, int ext_size);
 
-struct v_quic_socket *us_quic_socket_context_connect(struct v_quic_socket_context *context, const char *host, int port, int ext_size);
+void v_quic_socket_create_stream(struct v_quic_socket *s, int ext_size);
 
-void us_quic_socket_create_stream(struct v_quic_socket *s, int ext_size);
-
-struct v_quic_socket *us_quic_stream_socket(struct v_quic_stream *s);
+struct v_quic_socket *v_quic_stream_socket(struct v_quic_stream *s);
 
 /* This one is ugly and is only used to make clean examples */
-int us_quic_stream_is_client(struct v_quic_stream *s);
+int v_quic_stream_is_client(struct v_quic_stream *s);
 
-void us_quic_socket_context_on_stream_data(struct v_quic_socket_context *context,
-                                           void(*on_stream_data)(struct v_quic_stream *s, char *data, int length));
+void v_quic_socket_context_on_stream_data(struct v_quic_socket_context *context,
+                                          void(*on_stream_data)(struct v_quic_stream *s, char *data, int length));
 
-void us_quic_socket_context_on_stream_end(struct v_quic_socket_context *context,
-                                          void(*on_stream_data)(struct v_quic_stream *s));
+void v_quic_socket_context_on_stream_end(struct v_quic_socket_context *context, void(*on_stream_data)(struct v_quic_stream *s));
 
-void us_quic_socket_context_on_stream_headers(struct v_quic_socket_context *context,
-                                              void(*on_stream_headers)(struct v_quic_stream *s));
+void v_quic_socket_context_on_stream_headers(struct v_quic_socket_context *context, void(*on_stream_headers)(struct v_quic_stream *s));
 
-void us_quic_socket_context_on_stream_open(struct v_quic_socket_context *context,
-                                           void(*on_stream_open)(struct v_quic_stream *s, int is_client));
+void v_quic_socket_context_on_stream_open(struct v_quic_socket_context *context,
+                                          void(*on_stream_open)(struct v_quic_stream *s, int is_client));
 
-void us_quic_socket_context_on_stream_close(struct v_quic_socket_context *context,
-                                            void(*on_stream_close)(struct v_quic_stream *s));
+void v_quic_socket_context_on_stream_close(struct v_quic_socket_context *context, void(*on_stream_close)(struct v_quic_stream *s));
 
-void us_quic_socket_context_on_open(struct v_quic_socket_context *context,
-                                    void(*on_open)(struct v_quic_socket *s, int is_client));
+void v_quic_socket_context_on_open(struct v_quic_socket_context *context, void(*on_open)(struct v_quic_socket *s, int is_client));
 
-void us_quic_socket_context_on_close(struct v_quic_socket_context *context, void(*on_close)(struct v_quic_socket *s));
+void v_quic_socket_context_on_close(struct v_quic_socket_context *context, void(*on_close)(struct v_quic_socket *s));
 
-void us_quic_socket_context_on_stream_writable(struct v_quic_socket_context *context,
-                                               void(*on_stream_writable)(struct v_quic_stream *s));
+void v_quic_socket_context_on_stream_writable(struct v_quic_socket_context *context, void(*on_stream_writable)(struct v_quic_stream *s));
 
-void *us_quic_socket_context_ext(struct v_quic_socket_context *context);
+void *v_quic_socket_context_ext(struct v_quic_socket_context *context);
 
-struct v_quic_socket_context *us_quic_socket_context(struct v_quic_socket *s);
+struct v_quic_socket_context *v_quic_socket_context(struct v_quic_socket *s);
 
 #endif //NETWORK_QUIC_H
+
 #endif
