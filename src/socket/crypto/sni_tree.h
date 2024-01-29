971ed4be55509aae6cfffde40e07bfad52f0f39a
0x303133 <0x303133@proton.me>
2024-01-30 01:13:02 +0300

    fix(socket): Add guard

--- src/socket/crypto/sni_tree.h
+++ src/socket/crypto/sni_tree.h
@@ -1,10 +1,12 @@
 #ifndef VENOK_SNI_TREE_H
 #define VENOK_SNI_TREE_H
 
+#ifndef VENOK_NO_SSL
 void *sni_new();
 void sni_free(void *sni, void(*cb)(void *));
 int sni_add(void *sni, const char *hostname, void *user);
 void *sni_remove(void *sni, const char *hostname);
 void *sni_find(void *sni, const char *hostname);
+#endif
 
 #endif //VENOK_SNI_TREE_H
