15c5bdd38cb0769cd0a39ede6591bfd6f25cc6b2
0x303133 <0x303133@proton.me>
2024-01-29 18:11:06 +0300

    feat(socket): Add automatic loop selection

--- src/socket/structures.h
+++ src/socket/structures.h
@@ -11,6 +11,26 @@
 #define VENOK_SOCKET_DESCRIPTOR int
 #endif
 
+/* 512kb shared receive buffer */
+#define VENOK_RECV_BUFFER_LENGTH 524288
+/* A timeout granularity of 4 seconds means give or take 4 seconds from set timeout */
+#define VENOK_TIMEOUT_GRANULARITY 4
+/* 32 byte padding of receive buffer ends */
+#define VENOK_RECV_BUFFER_PADDING 32
+/* Guaranteed alignment of extension memory */
+#define VENOK_EXT_ALIGNMENT 16
+
+/* Decide what eventing system to use by default */
+#if !defined(VENOK_USE_EPOLL) && !defined(VENOK_USE_LIBUV) && !defined(VENOK_USE_KQUEUE)
+#if defined(_WIN32)
+#define VENOK_USE_LIBUV
+#elif defined(__APPLE__) || defined(__FreeBSD__)
+#define VENOK_USE_KQUEUE
+#else
+#define VENOK_USE_EPOLL
+#endif
+#endif
+
 struct v_socket;
 struct v_socket_context;
 struct v_loop;
