a35e975d84e6eda850b28ef4a76a7374306e8fa7
0x303133 <0x303133@proton.me>
2024-01-29 19:49:41 +0300

    feat(socket): Add to bsd TCP layer

--- src/socket/network/bsd.c
+++ src/socket/network/bsd.c
@@ -218,3 +218,223 @@ void *bsd_create_udp_packet_buffer() {
 }
 
 /* TCP Layer */
+VENOK_SOCKET_DESCRIPTOR apple_no_sigpipe(VENOK_SOCKET_DESCRIPTOR fd) {
+#ifdef __APPLE__
+    if (fd != VENOK_SOCKET_ERROR) {
+        int no_sigpipe = 1;
+        setsockopt(fd, SOL_SOCKET, SO_NOSIGPIPE, (void *) &no_sigpipe, sizeof(int));
+    }
+#endif
+    return fd;
+}
+
+VENOK_SOCKET_DESCRIPTOR bsd_set_nonblocking(VENOK_SOCKET_DESCRIPTOR fd) {
+#ifdef _WIN32
+    /* Libuv will set windows sockets as non-blocking */
+#else
+    fcntl(fd, F_SETFL, fcntl(fd, F_GETFL, 0) | O_NONBLOCK);
+#endif
+    return fd;
+}
+
+void bsd_socket_nodelay(VENOK_SOCKET_DESCRIPTOR fd, int enabled) {
+    setsockopt(fd, IPPROTO_TCP, TCP_NODELAY, (void *) &enabled, sizeof(enabled));
+}
+
+void bsd_socket_flush(VENOK_SOCKET_DESCRIPTOR fd) {
+    // Linux TCP_CORK has the same underlying corking mechanism as with MSG_MORE
+#ifdef TCP_CORK
+    int enabled = 0;
+    setsockopt(fd, IPPROTO_TCP, TCP_CORK, (void *) &enabled, sizeof(int));
+#endif
+}
+
+VENOK_SOCKET_DESCRIPTOR bsd_create_socket(int domain, int type, int protocol) {
+    // returns INVALID_SOCKET on error
+    int flags = 0;
+#if defined(SOCK_CLOEXEC) && defined(SOCK_NONBLOCK)
+    flags = SOCK_CLOEXEC | SOCK_NONBLOCK;
+#endif
+    VENOK_SOCKET_DESCRIPTOR created_fd = socket(domain, type | flags, protocol);
+    return bsd_set_nonblocking(apple_no_sigpipe(created_fd));
+}
+
+void bsd_close_socket(VENOK_SOCKET_DESCRIPTOR fd) {
+#ifdef _WIN32
+    closesocket(fd);
+#else
+    close(fd);
+#endif
+}
+
+void bsd_shutdown_socket(VENOK_SOCKET_DESCRIPTOR fd) {
+#ifdef _WIN32
+    shutdown(fd, SD_SEND);
+#else
+    shutdown(fd, SHUT_WR);
+#endif
+}
+
+void bsd_shutdown_socket_read(VENOK_SOCKET_DESCRIPTOR fd) {
+#ifdef _WIN32
+    shutdown(fd, SD_RECEIVE);
+#else
+    shutdown(fd, SHUT_RD);
+#endif
+}
+
+void internal_finalize_bsd_addr(struct bsd_addr *addr) {
+    // Parse, so to speak, the address
+    if (addr->mem.ss_family == AF_INET6) {
+        addr->ip = (char *) &((struct sockaddr_in6 *) addr)->sin6_addr;
+        addr->ip_length = sizeof(struct in6_addr);
+        addr->port = ntohs(((struct sockaddr_in6 *) addr)->sin6_port);
+    } else if (addr->mem.ss_family == AF_INET) {
+        addr->ip = (char *) &((struct sockaddr_in *) addr)->sin_addr;
+        addr->ip_length = sizeof(struct in_addr);
+        addr->port = ntohs(((struct sockaddr_in *) addr)->sin_port);
+    } else {
+        addr->ip_length = 0;
+        addr->port = -1;
+    }
+}
+
+int bsd_local_addr(VENOK_SOCKET_DESCRIPTOR fd, struct bsd_addr *addr) {
+    addr->len = sizeof(addr->mem);
+    if (getsockname(fd, (struct sockaddr *) &addr->mem, &addr->len)) return -1;
+    internal_finalize_bsd_addr(addr);
+    return 0;
+}
+
+int bsd_remote_addr(VENOK_SOCKET_DESCRIPTOR fd, struct bsd_addr *addr) {
+    addr->len = sizeof(addr->mem);
+    if (getpeername(fd, (struct sockaddr *) &addr->mem, &addr->len)) return -1;
+    internal_finalize_bsd_addr(addr);
+    return 0;
+}
+
+char *bsd_addr_get_ip(struct bsd_addr *addr) {
+    return addr->ip;
+}
+
+int bsd_addr_get_ip_length(struct bsd_addr *addr) {
+    return addr->ip_length;
+}
+
+int bsd_addr_get_port(struct bsd_addr *addr) {
+    return addr->port;
+}
+
+// Called by dispatch_ready_poll
+VENOK_SOCKET_DESCRIPTOR bsd_accept_socket(VENOK_SOCKET_DESCRIPTOR fd, struct bsd_addr *addr) {
+    VENOK_SOCKET_DESCRIPTOR accepted_fd;
+    addr->len = sizeof(addr->mem);
+
+#if defined(SOCK_CLOEXEC) && defined(SOCK_NONBLOCK)
+    // Linux, FreeBSD
+    accepted_fd = accept4(fd, (struct sockaddr *) addr, &addr->len, SOCK_CLOEXEC | SOCK_NONBLOCK);
+#else
+    // Windows, OS X
+    accepted_fd = accept(fd, (struct sockaddr *) addr, &addr->len);
+#endif
+
+    /* We cannot rely on addr since it is not initialized if failed */
+    if (accepted_fd == VENOK_SOCKET_ERROR) return VENOK_SOCKET_ERROR;
+
+    internal_finalize_bsd_addr(addr);
+
+    return bsd_set_nonblocking(apple_no_sigpipe(accepted_fd));
+}
+
+int bsd_recv(VENOK_SOCKET_DESCRIPTOR fd, void *buf, int length, int flags) {
+    return recv(fd, buf, length, flags);
+}
+
+#if !defined(_WIN32)
+#include <sys/uio.h>
+
+int bsd_write2(VENOK_SOCKET_DESCRIPTOR fd, const char *header, int header_length, const char *payload, int payload_length) {
+    struct iovec chunks[2];
+
+    chunks[0].iov_base = (char *)header;
+    chunks[0].iov_len = header_length;
+    chunks[1].iov_base = (char *)payload;
+    chunks[1].iov_len = payload_length;
+
+    return writev(fd, chunks, 2);
+}
+#else
+int bsd_write2(VENOK_SOCKET_DESCRIPTOR fd, const char *header, int header_length, const char *payload, int payload_length) {
+    int written = bsd_send(fd, header, header_length, 0);
+    if (written == header_length) {
+        int second_write = bsd_send(fd, payload, payload_length, 0);
+        if (second_write > 0) written += second_write;
+    }
+    return written;
+}
+#endif
+
+int bsd_send(VENOK_SOCKET_DESCRIPTOR fd, const char *buf, int length, int msg_more) {
+    // MSG_MORE (Linux), MSG_PARTIAL (Windows), TCP_NOPUSH (BSD)
+#ifndef MSG_NOSIGNAL
+#define MSG_NOSIGNAL 0
+#endif
+
+#ifdef MSG_MORE
+    // for Linux we do not want signals
+    return send(fd, buf, length, ((msg_more != 0) * MSG_MORE) | MSG_NOSIGNAL | MSG_DONTWAIT);
+#else
+    // use TCP_NOPUSH
+    return send(fd, buf, length, MSG_NOSIGNAL | MSG_DONTWAIT);
+#endif
+}
+
+int bsd_would_block() {
+#ifdef _WIN32
+    return WSAGetLastError() == WSAEWOULDBLOCK;
+#else
+    return errno == EWOULDBLOCK;// || errno == EAGAIN;
+#endif
+}
+
+inline __attribute__((always_inline)) VENOK_SOCKET_DESCRIPTOR bsd_bind_listen_fd(
+        VENOK_SOCKET_DESCRIPTOR listenFd,
+        struct addrinfo *listenAddr,
+        int port,
+        int options
+) {
+
+    if (port != 0) {
+        /* Otherwise, always enable SO_REUSEPORT and SO_REUSEADDR _unless_ options specify otherwise */
+#ifdef _WIN32
+        if (options & VENOK_LISTEN_EXCLUSIVE_PORT) {
+            int optval2 = 1;
+            setsockopt(listenFd, SOL_SOCKET, SO_EXCLUSIVEADDRUSE, (void *) &optval2, sizeof(optval2));
+        } else {
+            int optval3 = 1;
+            setsockopt(listenFd, SOL_SOCKET, SO_REUSEADDR, (void *) &optval3, sizeof(optval3));
+        }
+#else
+#if /*defined(__linux) &&*/ defined(SO_REUSEPORT)
+        if (!(options & VENOK_LISTEN_EXCLUSIVE_PORT)) {
+            int optval = 1;
+            setsockopt(listenFd, SOL_SOCKET, SO_REUSEPORT, (void *) &optval, sizeof(optval));
+        }
+#endif
+        int enabled = 1;
+        setsockopt(listenFd, SOL_SOCKET, SO_REUSEADDR, (void *) &enabled, sizeof(enabled));
+#endif
+    }
+
+#ifdef IPV6_V6ONLY
+    int disabled = 0;
+    setsockopt(listenFd, IPPROTO_IPV6, IPV6_V6ONLY, (void *) &disabled, sizeof(disabled));
+#endif
+
+    if (bind(listenFd, listenAddr->ai_addr, (socklen_t) listenAddr->ai_addrlen) || listen(listenFd, 512)) {
+        return VENOK_SOCKET_ERROR;
+    }
+
+    return listenFd;
+}
+
